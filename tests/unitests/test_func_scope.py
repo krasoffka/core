6f105e6cc48490bc8d24f04c72193186e21d204c
Sergey Krasochkin <sergey.krasochkin@fyst.com>
2024-10-14 13:56:08 +0300

    2

--- tests/unitests/test_func_scope.py
+++ tests/unitests/test_func_scope.py
@@ -1,23 +1,23 @@
-# import pytest
-# from sqlalchemy import text
-#
-#
-# @pytest.mark.asyncio(loop_scope='function')
-# async def test_unitest_dummy() -> None:
-#     assert True
-#
-#
-# @pytest.mark.asyncio(loop_scope='function')
-# async def test_unitest_session(session) -> None:
-#     result = await session.execute(text('SELECT 1'))
-#     row = result.scalar()
-#     print(f'Database connection is working, result: {row}')
-#     assert True
-#
-#
-# @pytest.mark.asyncio(loop_scope='function')
-# async def test_unitest_session2(session) -> None:
-#     result = await session.execute(text('SELECT 1'))
-#     row = result.scalar()
-#     print(f'Database connection is working, result: {row}')
-#     assert True
+import pytest
+from sqlalchemy import text
+
+
+@pytest.mark.asyncio(loop_scope='function')
+async def test_unitest_dummy() -> None:
+    assert True
+
+
+@pytest.mark.asyncio(loop_scope='function')
+async def test_unitest_session(session) -> None:
+    result = await session.execute(text('SELECT 1'))
+    row = result.scalar()
+    print(f'Database connection is working, result: {row}')
+    assert True
+
+
+@pytest.mark.asyncio(loop_scope='function')
+async def test_unitest_session2(session) -> None:
+    result = await session.execute(text('SELECT 1'))
+    row = result.scalar()
+    print(f'Database connection is working, result: {row}')
+    assert True
